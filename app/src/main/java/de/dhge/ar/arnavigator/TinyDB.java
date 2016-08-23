ceed71808c0d8e35360ba65db53d459e3265142c
Elias Fröhner <elias.froehner@gmx.de>
2016-08-23 17:40:20 +0200

    Remove image functions from TinyDB

--- app/src/main/java/de/dhge/ar/arnavigator/TinyDB.java
+++ app/src/main/java/de/dhge/ar/arnavigator/TinyDB.java
@@ -44,144 +44,9 @@ import android.util.Log;
 public class TinyDB {
 
     private SharedPreferences preferences;
-    private String DEFAULT_APP_IMAGEDATA_DIRECTORY;
-    private String lastImagePath = "";
 
     public TinyDB(Context appContext) {
         preferences = PreferenceManager.getDefaultSharedPreferences(appContext);
-    }
-
-
-    /**
-     * Decodes the Bitmap from 'path' and returns it
-     * @param path image path
-     * @return the Bitmap from 'path'
-     */
-    public Bitmap getImage(String path) {
-        Bitmap bitmapFromPath = null;
-        try {
-            bitmapFromPath = BitmapFactory.decodeFile(path);
-
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-        }
-
-        return bitmapFromPath;
-    }
-
-
-    /**
-     * Returns the String path of the last saved image
-     * @return string path of the last saved image
-     */
-    public String getSavedImagePath() {
-        return lastImagePath;
-    }
-
-
-    /**
-     * Saves 'theBitmap' into folder 'theFolder' with the name 'theImageName'
-     * @param theFolder the folder path dir you want to save it to e.g "DropBox/WorkImages"
-     * @param theImageName the name you want to assign to the image file e.g "MeAtLunch.png"
-     * @param theBitmap the image you want to save as a Bitmap
-     * @return returns the full path(file system address) of the saved image
-     */
-    public String putImage(String theFolder, String theImageName, Bitmap theBitmap) {
-        if (theFolder == null || theImageName == null || theBitmap == null)
-            return null;
-
-        this.DEFAULT_APP_IMAGEDATA_DIRECTORY = theFolder;
-        String mFullPath = setupFullPath(theImageName);
-
-        if (!mFullPath.equals("")) {
-            lastImagePath = mFullPath;
-            saveBitmap(mFullPath, theBitmap);
-        }
-
-        return mFullPath;
-    }
-
-
-    /**
-     * Saves 'theBitmap' into 'fullPath'
-     * @param fullPath full path of the image file e.g. "Images/MeAtLunch.png"
-     * @param theBitmap the image you want to save as a Bitmap
-     * @return true if image was saved, false otherwise
-     */
-    public boolean putImageWithFullPath(String fullPath, Bitmap theBitmap) {
-        return !(fullPath == null || theBitmap == null) && saveBitmap(fullPath, theBitmap);
-    }
-
-    /**
-     * Creates the path for the image with name 'imageName' in DEFAULT_APP.. directory
-     * @param imageName name of the image
-     * @return the full path of the image. If it failed to create directory, return empty string
-     */
-    private String setupFullPath(String imageName) {
-        File mFolder = new File(Environment.getExternalStorageDirectory(), DEFAULT_APP_IMAGEDATA_DIRECTORY);
-
-        if (isExternalStorageReadable() && isExternalStorageWritable() && !mFolder.exists()) {
-            if (!mFolder.mkdirs()) {
-                Log.e("ERROR", "Failed to setup folder");
-                return "";
-            }
-        }
-
-        return mFolder.getPath() + '/' + imageName;
-    }
-
-    /**
-     * Saves the Bitmap as a PNG file at path 'fullPath'
-     * @param fullPath path of the image file
-     * @param bitmap the image as a Bitmap
-     * @return true if it successfully saved, false otherwise
-     */
-    private boolean saveBitmap(String fullPath, Bitmap bitmap) {
-        if (fullPath == null || bitmap == null)
-            return false;
-
-        boolean fileCreated = false;
-        boolean bitmapCompressed = false;
-        boolean streamClosed = false;
-
-        File imageFile = new File(fullPath);
-
-        if (imageFile.exists())
-            if (!imageFile.delete())
-                return false;
-
-        try {
-            fileCreated = imageFile.createNewFile();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        FileOutputStream out = null;
-        try {
-            out = new FileOutputStream(imageFile);
-            bitmapCompressed = bitmap.compress(CompressFormat.PNG, 100, out);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            bitmapCompressed = false;
-
-        } finally {
-            if (out != null) {
-                try {
-                    out.flush();
-                    out.close();
-                    streamClosed = true;
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    streamClosed = false;
-                }
-            }
-        }
-
-        return (fileCreated && bitmapCompressed && streamClosed);
     }
 
     // Getters
@@ -482,16 +347,6 @@ public class TinyDB {
     public void remove(String key) {
         preferences.edit().remove(key).apply();
     }
-
-    /**
-     * Delete image file at 'path'
-     * @param path path of image file
-     * @return true if it successfully deleted, false otherwise
-     */
-    public boolean deleteImage(String path) {
-        return new File(path).delete();
-    }
-
 
     /**
      * Clear SharedPreferences (remove everything)
