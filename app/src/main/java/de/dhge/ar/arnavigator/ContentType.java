b66e4c988d38a8337714dd60c1b060c44a7663e1
Elias Fröhner <elias.froehner@gmx.de>
2016-08-23 20:48:25 +0200

    Added content type

---
+++ app/src/main/java/de/dhge/ar/arnavigator/ContentType.java
@@ -0,0 +1,15 @@
+package de.dhge.ar.arnavigator;
+
+/**
+ * Class which holds possible content types
+ */
+
+public class ContentType {
+    public static String ROOM = "ROOM";
+    public static String MAP = "MAP";
+    public static String EXIT = "EXIT";
+    public static String STAIRS_UP = "STAIRS_UP";
+    public static String STAIRS_DOWN = "STAIRS_DOWN";
+    public static String ADJUSTMENT_POINT = "ADJUSTMENT_POINT";
+    public static String ONLINE_TARGET = "ONLINE_TARGET";
+}
