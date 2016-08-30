703aebe9660df006a9028a5b69085d76fbdc7a12
Elias Fröhner <elias.froehner@gmx.de>
2016-08-30 09:54:54 +0200

    getID works now as expected

--- app/src/main/java/de/dhge/ar/arnavigator/util/ContentParser.java
+++ app/src/main/java/de/dhge/ar/arnavigator/util/ContentParser.java
@@ -51,7 +51,7 @@ public class ContentParser {
      * @return String type
      */
     public String getID() {
-        return getCustomNode("type");
+        return getCustomNode("id");
     }
 
     /**
